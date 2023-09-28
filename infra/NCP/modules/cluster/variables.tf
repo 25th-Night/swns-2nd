150dc0882867d9137dd6f6756ba505516a5bdc31
25th-Night <brown_eyed87@naver.com>
2023-09-28 09:54:51 +0900

    infra/NCP/modules/cluster: cluster 모듈 추가

---
+++ infra/NCP/modules/cluster/variables.tf
@@ -0,0 +1,31 @@
+variable "ncp_access_key" {
+  type = string
+}
+
+variable "ncp_secret_key" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "lb_subnet_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "k8s_version" {
+  type = string
+}
